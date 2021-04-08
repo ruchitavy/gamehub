d57283d28b275d7412b3059b7907a78de944ef8f
rushabh.y <rushabh.y@media.net>
2021-04-08 21:24:49 +0530

    Add flask

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, send_from_directory
+from flask_login import login_required
+
+app = Flask(__name__, static_url_path='/static')
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index-2.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/feedback')
+@login_required
+def feedback():
+    return render_template('Feedback.html')
+
+
+@app.route('/games/<game>')
+def run_game(game):
+    return render_template('/games/' + game + '/index.html', game=game)
+
+
+app.run()
